4d448b46ef2bacc4db66fe348296a531ca5b89fc
fernando <fernandomercado529@gmail.com>
2022-08-30 16:06:46 -0400

    Add new properties

--- src/components/MyLabel/MyLabel.tsx
+++ src/components/MyLabel/MyLabel.tsx
@@ -1,15 +1,33 @@
 
 import './mylabel.css'
 export interface MyLabelProps {
+   /**
+   * This is the text to show in tag
+   */
   label: string
+  /**
+   * This is the size to text in tag
+   */
   size: 'normal' | 'h1' | 'h2' | 'h3'
+  color:'primary' | 'secondary' | 'tertiary'
+  allCaps:boolean
+  /**
+   * Custom color font
+   */
+  fontColor?:string
 }
 export const MyLabel = ({
   label = 'No label',
-  size = 'normal'
+  size = 'normal',
+  allCaps=false,
+  color ='primary',
+  fontColor
+
 }: MyLabelProps) => {
-  return <span className={`${size}`}>
-    {label}
+  return <span className={`${size} label text-${color}`}
+    style={{color:fontColor}}
+  >
+    {allCaps? label.toUpperCase():label}
   </span>
 
 }
--- src/stories/components/MyLabel.stories.tsx
+++ src/stories/components/MyLabel.stories.tsx
@@ -6,7 +6,11 @@ import { MyLabel } from "../../components/MyLabel/MyLabel";
 //exportacion por defecto
 export default {
     title:'UI/Labels/MyLabel',
-    component:MyLabel
+    component:MyLabel,
+    argTypes:{
+        size:{control:'select'},
+        color:{control:'select'}
+    }
 } as ComponentMeta<typeof MyLabel>
 
 
@@ -17,6 +21,38 @@ const Template:ComponentStory<typeof MyLabel> =(args)=><MyLabel {...args}  />
 
 //Crear submenus
 export const Basic = Template.bind({})
+Basic.args ={
+    size:'normal',
+    allCaps:false
+
+}
 export const AllCaps= Template.bind({})
+AllCaps.args ={
+    size:'normal',
+    allCaps:true
+}
 export const Secondary= Template.bind({})
 
+Secondary.args ={
+    size:'normal',
+    allCaps:false,
+    color:'secondary'
+
+}
+
+export const Tertiary= Template.bind({})
+
+Tertiary.args ={
+    size:'normal',
+    allCaps:false,
+    color:'tertiary'
+
+}
+
+
+export const CustomFontColor= Template.bind({})
+
+CustomFontColor.args ={
+    size:'h1',
+    fontColor:'#333'
+}
